63687663b90dcc90e303c399cd8d2c619e568773
Snakepit7 <julienfollet71@gmail.com>
2018-10-09 16:02:09 +0200

    Mise à jour

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-function button(){
-  var prems = parseInt(document.getElementById("firstNumber").value);
-  var second = Number(document.getElementById("secondNumber").value);
-  var total = Number(prems * second);
-  document.getElementById("total").value = total;
-  alert("total : " + total);
-}
+
