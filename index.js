dd572cbd7c5d42df6e8096e7c82ee1448bcf35e1
Snakepit7 <julienfollet71@gmail.com>
2018-10-10 09:25:06 +0200

    Mise à jour

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//regex//
+function reg0(valeur) {
+var reg0 = new RegExp ('^')
+}
 
+
+
+// fonction qui se déclenche lorsque l'on clique sur le boutton //
+function button(){
+  var firstnumb = document.getElementById("firstNumber").value;
+  var secondnumb = document.getElementById("secondNumber").value;
+}
